d81cc6d8af5f7f2ecd446407b56fc9b6be6c6762
Benslimane Houcine <45845838+HocineBenslimane@users.noreply.github.com>
2023-07-04 17:47:34 +0100

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -269,7 +269,7 @@
           <h3 class="mt-12">Hello 👋 Welcome to my portfolio!</h3>
           <h4 class="text-3xl mt-2 md:mt-0 md:text-5xl">
             I'm
-            <span class="capitalize text-5xl xl:text-6xl font-black">Houcine Benslimane</span>
+            <span class="capitalize text-5xl xl:text-6xl font-black"> Benslimane</span>
           </h4>
           <p class="mt-4 text-md md:text-1xl max-w-sm md:max-w-xl">
             Fullstack developer and a Designer from
